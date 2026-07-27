3829aa6fba934c961ea1e02e8f409d22e6cc7bd5
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2026-07-27 16:45:16 +0700

    feat: integrate PWA support and update PomodoroApp component
    - Added VitePWA plugin to enable PWA capabilities with auto-update registration.
    - Configured PWA manifest with app details and icons for better user experience.
    - Commented out the entire PomodoroApp component code for potential future use or refactoring.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,39 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "StudyMate - Pomodoro Timer",
+        short_name: "StudyMate",
+        description: "A focused Pomodoro timer for studying",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "stopwatch-svgrepo-com-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "stopwatch-svgrepo-com-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
