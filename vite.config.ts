6641b1f4afa967be103242602ace33dd94b2d5f2
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2026-07-28 16:53:48 +0700

    Update favicon and title for improved branding; add new stopwatch icons
    - Changed the title in index.html from "study-mate-ai" to "StudyMate AI" for better branding.
    - Added a new favicon file (favicon-v1.svg) to enhance the visual identity of the application.
    - Updated the existing favicon.svg with a new SVG structure and design.
    - Introduced new stopwatch icon images (stopwatch-svgrepo-com-192x192-3.png and stopwatch-svgrepo-com-512x512-3.png) for better resolution and clarity.
    - Updated vite.config.ts to reference the new stopwatch icon files.

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,12 @@ export default defineConfig({
 
         icons: [
           {
-            src: "stopwatch-svgrepo-com-192x192-2.png",
+            src: "stopwatch-svgrepo-com-192x192-3.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "stopwatch-svgrepo-com-512x512-2.png",
+            src: "stopwatch-svgrepo-com-512x512-3.png",
             sizes: "512x512",
             type: "image/png",
           },
